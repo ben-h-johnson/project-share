6b1ecc24a7759112a4b7da4ad901fa2a7865cd2a
Benjamin Johnson <bjohnson@Benjamins-MacBook-Air.local>
2023-08-21 12:18:23 -0600

    corrected styling on about page, added image, updated content, began building top nav on app component

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Outlet } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import bjIcon from './images/favicon-32x32.png';
+// import { Paper } from '@mui/material';
+// import MenuIcon from '@mui/icons-material/Menu';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  components: {
+    // Name of the component
+    MuiCard: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          // padding: 10,
+          margin: 5,
+          maxWidth: 900,
+        },
+      },
+    },
+  },
+});
+
+const SimpleAppBar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Avatar alt="Hire Ben Johnson" src={bjIcon} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Hire Ben Johnson
+          </Typography>
+          <IconButton 
+            component="a" 
+            href="https://www.linkedin.com/in/ben-johnson-18984510"
+            target="_blank"
+            rel="noopener noreferrer">
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton 
+            component="a" 
+            href="https://github.com/ben-h-johnson/project-share"
+            target="_blank"
+            rel="noopener noreferrer">
+            <GitHubIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <SimpleAppBar />
+      <Outlet />
+    </ThemeProvider>
   );
 }
 
--- web/src/components/About.js
+++ web/src/components/About.js
@@ -5,6 +5,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Card from "@mui/material/Card";
+import CardContent from '@mui/material/CardContent';
+import { CardMedia } from "@mui/material";
+import hikeImage from '../images/benbarelyhike.jpeg';
+// import CardMedia from '@mui/material/CardMedia';
 
 const darkTheme = createTheme({
   palette: {
@@ -17,9 +21,9 @@ const darkTheme = createTheme({
         // Name of the slot
         root: {
           // Some CSS
-          padding: 10,
+          // padding: 10,
           margin: 5,
-          maxWidth: 800,
+          maxWidth: 900,
         },
       },
     },
@@ -33,46 +37,57 @@ const About = () => {
       <Grid container spacing={1}>
         <Grid xs={12} sm={9}>
           <Card>
-            <Typography id="welcome" variant="h4">Welcome</Typography>
-            <Typography paragraph={true}>If you're here, it's likely I applied to work with you. Thanks taking the time to consider my application. This website is intended to demonstrate a baseline of technical ability as well as to provide some additional information about me. Currently I am studying Machine Learning and will share any projects here. Please check back soon.</Typography>
+            <CardContent>
+              <Typography id="welcome" variant="h4">Welcome</Typography>
+              <Typography paragraph={true} variant="body1">If you're here, it's likely I applied to work with you. Thanks taking the time to consider my application. This website is intended to demonstrate a baseline of technical ability as well as to provide some additional information about me. Currently I am taking courses on machine learning and will share any projects here. Please check back soon.</Typography>
+            </CardContent>
           </Card><Card>
-            <Typography variant="h4" id="aboutThisSite">About this website</Typography>
-            <Typography variant="h5" id="frontEnd">Front End</Typography>
-            <Typography paragraph={true}>This website is built using React. While React is unnecessary for something this simple, it provides an extensible framework for adding application components down the road. More importantly, it provides client side routing. <Link 
-              href="https://mui.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >Material UI</Link> is used for styling and components.  You can find the <Link 
-              href="https://github.com/ben-h-johnson/project-share"
-              target="_blank"
-              rel="noopener noreferrer"
-            >source code here.</Link></Typography>
-            <Typography variant="h5" id="hosting">Hosting</Typography>
-            <Typography paragraph={true}>This site is hosted using Amazon Web Services (AWS) as a static website on S3. AWS Route53 was used as the DNR. AWS ACM provides the SSL cert. An AWS Cloudfront distribution is necessary to support the certificate.</Typography>
-            <Typography variant="h5" id="cost">Cost</Typography>
-            <Typography paragraph={true}>$5/yr for the domain. Less than $1/mo for the R53 Hosted Zone and S3+Cloudfront traffic.</Typography>
-          </Card><Card>
-            <Typography variant="h4" id="aboutMe">About me</Typography>
-            <Typography variant="h5" id="professionally">Professional Experience</Typography>
-            <Typography paragraph={true}>I have over 10 years of experience in customer facing technical roles within the B2B Software as a Service industry. Most of my direct experience is in sales and evangelism, or with integrations, both data and transactional (via non-restful API services). Prior to that I served 5 years in the US Navy, working as a project engineer on control systems for the Navy Nuclear Propulsion Program. I studied Electrical Engineering at Georgia Tech.</Typography>
-            <Typography variant="h5" id="certs">Active Certifications</Typography>
-            <Typography paragraph={true}>Amazon Web Services (AWS) Solutions Architect Associate - certified August 2023</Typography>
-            <Typography variant="h5" id="courses">Coursework</Typography>
-            <Typography paragraph={true}>
-              <Link href="https://www.udemy.com/course/deeplearning_x"
+            <CardContent>
+              <Typography variant="h4" id="aboutThisSite">About this website</Typography>
+              <Typography variant="h5" id="frontEnd">Front End</Typography>
+              <Typography paragraph={true} variant="body1">This website is built using React. While React is unnecessary for something this simple, it provides an extensible framework for adding application components down the road. Equally important, it provides client side routing. <Link 
+                href="https://mui.com/"
                 target="_blank"
                 rel="noopener noreferrer"
-              >A deep understanding of deep learning</Link> - in progress
-            </Typography>
-            <Typography paragraph={true}>
-              <Link href="https://www.coursera.org/learn/generative-ai-with-llms"
+              >Material UI</Link> is used for styling and components.  You can find the <Link 
+                href="https://github.com/ben-h-johnson/project-share"
                 target="_blank"
                 rel="noopener noreferrer"
-              >Generative AI with Large Language Models</Link> - next course planned
-            </Typography>
-            <Typography paragraph={true}>Please Note: I will likely adapt this plan to future employment</Typography>
-            <Typography variant="h5" id="personally">Personal</Typography>
-            <Typography paragraph={true}>I live in Boulder, CO. I enjoy learning and reading about a variety of subjects both within the realm of technology (AI/ML), and people (psycology, anthropology, theology, philosophy), and enjoy discussing their intersection.</Typography>
+              >source code here.</Link></Typography>
+              <Typography variant="h5" id="hosting">Hosting</Typography>
+              <Typography paragraph={true} variant="body1">This site is hosted using Amazon Web Services (AWS) as a static website on S3. AWS Route53 was used as the DNR. AWS ACM provides the SSL cert. An AWS Cloudfront distribution is necessary to support the certificate.</Typography>
+              <Typography variant="h5" id="cost">Cost</Typography>
+              <Typography paragraph={true} variant="body1">$5/yr for the domain. Less than $1/mo for the R53 Hosted Zone and S3+Cloudfront traffic.</Typography>
+            </CardContent>
+          </Card><Card>
+            <CardContent>
+              <Typography variant="h4" id="aboutMe">About me</Typography>
+              <Typography variant="h5" id="professionally">Professional Experience</Typography>
+              <Typography paragraph={true} variant="body1">I have over 10 years of experience in customer facing technical roles within the B2B Software as a Service industry. Most of my direct experience is in solutions or in building SaaS integrations, both data (e.g. ETL) and transactional (via non-restful API services). Prior to that I served 5 years in the US Navy, working as a project engineer on control systems for the Navy Nuclear Propulsion Program. I studied Electrical Engineering at Georgia Tech.</Typography>
+              <Typography variant="h5" id="certs">Active Certifications</Typography>
+              <Typography paragraph={true} variant="body1">Amazon Web Services (AWS) Solutions Architect Associate - certified August 2023</Typography>
+              <Typography variant="h5" id="courses">Coursework</Typography>
+              <Typography paragraph={true} variant="body1">
+                <Link href="https://www.udemy.com/course/deeplearning_x"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >A deep understanding of deep learning</Link> - in progress
+              </Typography>
+              <Typography paragraph={true} variant="body1">
+                <Link href="https://www.coursera.org/learn/generative-ai-with-llms"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >Generative AI with Large Language Models</Link> - next course planned
+              </Typography>
+              <Typography paragraph={true} variant="body1">Please Note: I will likely adapt this plan to future employment</Typography>
+              <Typography variant="h5" id="personally">Personal</Typography>
+              <Typography paragraph={true} variant="body1">I live in Boulder, CO. I enjoy reading about and discussing a wide variety of subjects. Currently, I am spending most of this energy on professional development. I also enjoy hiking, yoga, cooking, and my dog.</Typography>
+              <CardMedia
+                component="img"
+                image={hikeImage}
+                alt="hiking with the dog"
+              />
+            </CardContent>
           </Card>
         </Grid>
         <Grid xs={0} sm={3}>
